0c9caf13105d577c1af53d1faeaf9766924e667c
DmytroKovinskyi <163335412+DmytroKovinskyi@users.noreply.github.com>
2024-04-01 14:25:08 +0300

    Laba 4

---
+++ Laba-4/Laba-4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace VirtualMethodsExample
+{
+    class Transport
+    {
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine("Transport: Generic");
+        }
+    }
+
+    class Car : Transport
+    {
+        public override void ShowInfo()
+        {
+            Console.WriteLine("Transport: Car");
+        }
+    }
+
+    class Plane : Transport
+    {
+        public override void ShowInfo()
+        {
+            Console.WriteLine("Transport: Plane");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Transport vehicle;
+
+            Console.WriteLine("Choose a vehicle:");
+            Console.WriteLine("1. Car");
+            Console.WriteLine("2. Plane");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                vehicle = new Car();
+            }
+            else if (choice == 2)
+            {
+                vehicle = new Plane();
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice!");
+                return;
+            }
+
+            Console.WriteLine("Information about the chosen vehicle:");
+            vehicle.ShowInfo();
+        }
+    }
+}
