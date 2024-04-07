c3922cc953e3ce16bfc3b8cb0b8682381a62a0df
Andrew <shikigamori@gmail.com>
2024-04-08 02:49:53 +0300

    Закінчив лабу 4 (8 варіант)

---
+++ Laba4/Laba4/Parallelepiped.cs
@@ -0,0 +1,24 @@
+﻿namespace Laba4;
+
+internal class Parallelepiped : Rectangle
+{
+    private double a3;
+    private double b3;
+
+    public Parallelepiped(double a1, double a2, double a3, double b1, double b2, double b3) : base(a1, a2, b1, b2)
+    {
+        this.a3 = a3;
+        this.b3 = b3;
+    }
+
+    public override void PrintCoefficients()
+    {
+        Console.WriteLine($"a1: {a1}, a2: {a2}, a3: {a3}, b1: {b1}, b2: {b2}, b3: {b3}");
+    }
+
+
+    public bool IsPointInsideByThreeCoordinates(double x, double y, double z)
+    {
+        return base.IsPointInsideByTwoCoordinates(x, y) && z >= b3 && z <= a3;
+    }
+}
