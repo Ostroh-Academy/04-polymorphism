c3d8d66811c24615567bb1a22c7a9de43f9db063
Анастасія Черній <jesikafan@gmail.com>
2024-04-07 22:20:01 +0300

    Лаб.№4 А.Черній

---
+++ Matrix.cs
@@ -0,0 +1,36 @@
+﻿namespace MatrixApp
+{
+    abstract class Matrix
+    {
+        protected int[] dimensions;
+
+        public Matrix(params int[] dimensions)
+        {
+            this.dimensions = dimensions;
+        }
+
+        public virtual void Set()
+        {
+            throw new NotImplementedException("Set method must be overridden in derived classes.");
+        }
+
+        public virtual void Random()
+        {
+            throw new NotImplementedException("Random method must be overridden in derived classes.");
+        }
+
+        public abstract int Min();
+        public static Matrix CreateMatrix(int dimensions)
+        {
+            switch (dimensions)
+            {
+                case 2:
+                    return new Matrix2D(3, 3);
+                case 3:
+                    return new Matrix3D(3, 3, 3);
+                default:
+                    throw new ArgumentException("Матрицi вказаної розмiностi не пiдтримуються");
+            }
+        }
+    }
+}
---
+++ Matrix2D.cs
@@ -0,0 +1,54 @@
+﻿namespace MatrixApp
+{
+    class Matrix2D : Matrix
+    {
+        private int[,] matrix;
+
+        public Matrix2D(int rows, int cols) : base(rows, cols)
+        {
+            matrix = new int[rows, cols];
+        }
+
+        public override void Set()
+        {
+            Console.WriteLine("Введiть елементи двовимiрної матрицi:");
+            for (int i = 0; i < dimensions[0]; i++)
+            {
+                for (int j = 0; j < dimensions[1]; j++)
+                {
+                    Console.Write($"matrix[{i},{j}] = ");
+                    matrix[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+
+        public override void Random()
+        {
+            Random random = new Random();
+            for (int i = 0; i < dimensions[0]; i++)
+            {
+                for (int j = 0; j < dimensions[1]; j++)
+                {
+                    matrix[i, j] = random.Next(-100, 101);
+                    Console.Write($"A[{i},{j}] = {matrix[i, j]}\n");
+                }
+            }
+        }
+
+        public override int Min()
+        {
+            int minElement = matrix[0, 0];
+            for (int i = 0; i < dimensions[0]; i++)
+            {
+                for (int j = 0; j < dimensions[1]; j++)
+                {
+                    if (matrix[i, j] < minElement)
+                    {
+                        minElement = matrix[i, j];
+                    }
+                }
+            }
+            return minElement;
+        }
+    }
+}
