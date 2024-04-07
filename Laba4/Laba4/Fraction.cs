24293a3e58be7cdf6a513b1d192803c279bf266e
Valera Sloza <valera.sloza@gmail.com>
2024-04-08 00:59:18 +0300

    Done

---
+++ Laba4/Laba4/Fraction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba4
+{
+    public class Fraction
+    {
+        protected double a;
+
+        public Fraction(double coefficient)
+        {
+            a = coefficient;
+        }
+
+        public virtual void SetCoefficient(double coefficient)
+        {
+            a = coefficient;
+        }
+
+        public virtual void PrintCoefficient()
+        {
+            Console.WriteLine("Coefficient a: " + a);
+        }
+
+        public virtual double CalculateFractionValue(double x)
+        {
+            return 1 / (a * x);
+        }
+    }
+}
---
+++ Laba4/Laba4/ThreeDimensionalFraction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba4
+{
+    public class ThreeDimensionalFraction : Fraction
+    {
+        private double a2;
+        private double a3;
+
+        public ThreeDimensionalFraction(double coefficient, double coefficient2, double coefficient3) : base(coefficient)
+        {
+            a2 = coefficient2;
+            a3 = coefficient3;
+        }
+
+        public override void SetCoefficient(double coefficient)
+        {
+            base.SetCoefficient(coefficient);
+        }
+
+        public override void PrintCoefficient()
+        {
+            base.PrintCoefficient();
+            Console.WriteLine("Coefficient a2: " + a2);
+            Console.WriteLine("Coefficient a3: " + a3);
+        }
+
+        public override double CalculateFractionValue(double x)
+        {
+            return 1 / (a * x + 1 / (a2 * x + 1 / a3 * x));
+        }
+    }
+}
